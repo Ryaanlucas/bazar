2274aff0c93c6ed8f46488464fc4159f545335c7
Ryaanlucas <84087469+Ryaanlucas@users.noreply.github.com>
2025-05-05 15:54:14 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -277,7 +277,19 @@ export default function App() {
   );
 
   return (
-    <div style={{ backgroundColor: darkMode ? '#1d1d1d' : '#a3b18a', color: darkMode ? '#fff' : '#000', minHeight: '100vh', padding: '2rem', maxWidth: '1200px', margin: '0 auto' }}>
+    <div
+  style={{
+    backgroundImage: 'url(/bg-textura.png)',
+    backgroundSize: 'cover',
+    backgroundRepeat: 'repeat',
+    backgroundPosition: 'center',
+    minHeight: '100vh',
+    padding: '2rem',
+    maxWidth: '1200px',
+    margin: '0 auto',
+    color: darkMode ? '#fff' : '#000'
+  }}
+>
       <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', marginBottom: '1rem' }}>
         <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', marginBottom: '1.5rem' }}>
   <img src="/logo.png" alt="Logo Thrift" style={{ width: '120px', marginBottom: '1rem' }} />
