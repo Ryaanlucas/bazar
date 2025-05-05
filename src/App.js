101dc91cdfe777f00adbf782386e3df8d7caf33b
Ryaanlucas <84087469+Ryaanlucas@users.noreply.github.com>
2025-05-05 16:34:59 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -342,7 +342,7 @@ export default function App() {
               <p style={{ fontWeight: 'bold', fontSize: '1.125rem', color: '#4e6b1a' }}>{item.price}</p>
               {!item.sold ? (
                 <a
-                  href="https://wa.me/5592993770892?text=Ol%C3%A1%2C%20estava%20olhando%20seu%20bazar%20e%20me%20interessei%20por%20alguns%20itens%20%F0%9F%98%8A%2C%20poderia%20conversar%3F."
+                  href="https://wa.me/5592993770892?text=Ol%C3%A1%2C%20estava%20olhando%20seu%20bazar%20e%20me%20interessei%20por%20alguns%20itens%20%F0%9F%98%8A%2C%20poderia%20conversar%3F"
                   target="_blank"
                   rel="noopener noreferrer"
                   style={{ display: 'inline-block', marginTop: '1rem', backgroundColor: '#7cbb00', color: 'white', padding: '0.75rem 1.25rem', borderRadius: '0.75rem', textAlign: 'center', textDecoration: 'none', fontSize: '1rem' }}
